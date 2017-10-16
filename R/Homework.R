1f363ea53490efc585e7796cb5fb030465f6210f
Raphael-Selignan <raphael.selignan@gmail.com>
2017-10-16 16:42:33 +0200

    General documentation

---
+++ R/Homework.R
@@ -0,0 +1,8 @@
+#' Stores 5 functions for database manipulation
+#'
+#' Enable to load, save and modify excel, csv files, and to work on the French first name database
+#'
+#' @name Homework
+#' @docType package
+#' @author Raphael Selignan <raphael.selignan@polytechnique.edu>
+NULL
